8804012ee3eae72f618fe642998a0f4cb6ff66b2
Jason Cheng <jasoncky96@gmail.com>
2020-05-23 15:53:27 +0800

    added model architecture for DCGAN, and changed dataset folder name

--- models/dcgan.py
+++ models/dcgan.py
@@ -19,8 +19,57 @@ class Generator(nn.Module):
         nc = config['nc']
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(in_channels=nz, out_channels=ngf*8, kernel_size=4, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True)
+            nn.ReLU(True),
+            # state size: (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(in_channels=ngf*8, out_channels=ngf*4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),     
+            # state size: (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(in_channels=ngf*4, out_channels=ngf*2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),     
+            # state size: (ngf*4) x 16 x 16
+            nn.ConvTranspose2d(in_channels=ngf*2, out_channels=ngf, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),     
+            # state size: (ngf*4) x 32 x 32
+            nn.ConvTranspose2d(in_channels=ngf, out_channels=nc, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()
+            # state size: (nc) x 64 x 64
         )
-        F.conv_transpose2d()
+    
+    def forward(self, input):
+        return self.main(input)
+
+class Discriminator(nn.Module):
+    def __init__(self, config):
+        super(Discriminator, self).__init__()
+        self.ngpu = config['ngpu']
+        ndf = config['ndf']
+        nc = config['nc']
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(nc, ndf, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf) x 32 x 32
+            nn.Conv2d(ndf, ndf*2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf*2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf*2) x 16 x 16
+            nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf*4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf*4) x 8 x 8
+            nn.Conv2d(ndf*4, ndf*8, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ndf*8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size: (ndf*8) x 4 x 4
+            nn.Conv2d(ndf*8, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+            # output: 1x1x1
+        )
+
+    def forward(self, input):
+        return self.main(input)
